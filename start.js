31dcf094fe6b5aa77464012b2ef02750a174e14f
Emil Norén <eminor992@gmail.com>
2019-05-20 19:36:39 +0200

    upgrade4

--- start.js
+++ start.js
@@ -5,11 +5,27 @@ if (!fs.existsSync("botconfig.json")) {
 	console.log("Missing configuration file: 'botconfig.json'");
 	process.exit(0);
 }
-const botConfig = JSON.parse(fs.readFileSync("botconfig.json"));
+let botConfig = JSON.parse(fs.readFileSync("botconfig.json"));
+
+if (process.env.discordToken) {
+	botConfig.discordToken = process.env.discordToken;
+}
+if (process.env.databasePrivateKey) {
+	botConfig.databasePrivateKey = process.env.databasePrivateKey;
+}
+
+if (fs.existsSync("botsecrets.json")) {
+	botConfig = {
+		...botConfig,
+		...JSON.parse(fs.readFileSync("botsecrets.json"))
+	};
+}
 
 //flags
 const i = process.argv.indexOf("-t");
 botConfig.discordToken = i === -1 ? botConfig.discordToken : process.argv[i + 1];
+const d = process.argv.indexOf("-d");
+botConfig.databasePrivateKey = d === -1 ? botConfig.databasePrivateKey : process.argv[d + 1];
 
 //run
 const Bot = require("./out/bot");
